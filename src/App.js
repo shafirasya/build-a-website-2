b38f67b93047f6dff968262713e6bc2510e3e497
unknown <Administrator@KP0101031700060.pertamina.com>
2020-12-29 11:00:53 +0700

    add img1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import img1 from './assets/img1.jpg';
 
 function App() {
   return (
     <div className="App bg-blue-200 h-screen flex items-center justify-center">
       <header className="App-header">
+        <div>
+          <img src={img1} />
+        </div>
         <p className="text-2xl">
           Edit <code>src/Home.js</code> and save to reload.
         </p>
