4327e5145865ed0aedb4c657394434d2194bc353
unknown <Administrator@KP0101031700060.pertamina.com>
2020-12-30 12:17:04 +0700

    add cityjs dll

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,10 +5,22 @@ import React, { useState, useEffect, useMemo } from "react";
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
+import City from "../components/City";
+
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
+  return (
+    // Container
+    <div className="flex flex-col h-screen bg-green-200">
+      <City cityName={"Tokyo"} temp={"10°C"} color={"bg-yellow-500"} />
+
+      <City cityName={"Jakarta"} temp={"24°C"} color={"bg-red-500"} />
+
+      <City cityName={"Bandung"} temp={"17°C"} color={"bg-blue-500"} />
+    </div>
+  );
 }
 
 export default Home;
